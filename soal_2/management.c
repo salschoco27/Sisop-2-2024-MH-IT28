c53acc7e94bd5a8b7fb6e5ce1a8578bfafb6a271
salsa <151063684+salschoco27@users.noreply.github.com>
2024-04-20 20:51:13 +0700

    Update management.c

--- soal_2/management.c
+++ soal_2/management.c
@@ -1,1 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
